a00d01e763414a60cf0af785658681ed783514a4
KabhiCodeKabhiFork <20dcs004@lnmiit.ac.in>
2023-12-19 16:46:25 +0530

    Controls added

---
+++ js/controls.js
@@ -0,0 +1,47 @@
+class Controls{
+    constructor(){
+        this.forward = false;
+        this.backward = false;
+        this.left = false;
+        this.right = false;
+
+        this.#addListeners(); // This is a private method
+    }
+
+    #addListeners(){
+      document.onkeydown = (e) => {
+        switch(e.key){
+            case 'ArrowUp':
+                this.forward = true;
+                break;
+            case 'ArrowDown':
+                this.backward = true;
+                break;
+            case 'ArrowLeft':
+                this.left = true;
+                break;
+            case 'ArrowRight':
+                this.right = true;
+                break;
+        }
+        console.table(this)   
+        //This will provide a table in the console allowing you to see if the keys are being pressed
+      }   
+      document.onkeyup = (e) => {
+        switch(e.key){
+            case 'ArrowUp':
+                this.forward = false;
+                break;
+            case 'ArrowDown':
+                this.backward = false;
+                break;
+            case 'ArrowLeft':
+                this.left = false;
+                break;
+            case 'ArrowRight':
+                this.right = false;
+                break;
+        }
+    }
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-myCanvas.height = window.innerHeight;
+myCanvas.height = window.innerHeight; //This is not a good practice to use myCanvas just like this because myCanvas is a global variable and if it is modified elsewhere it can change here too
 myCanvas.width = 200;
 
 const ctx = myCanvas.getContext('2d');
-const car = new Car(100,100,30,50);
-car.draw(ctx)
+const car = new Car(100,400,30,50);
 
+animate();
+
+function animate(){//This function refreshes 60 times per second
+     
+    myCanvas.height = window.innerHeight; //This is not a good practice to use myCanvas just like this because myCanvas is a global variable and if it is modified elsewhere it can change here too
+    car.update();
+    car.draw(ctx);
+    requestAnimationFrame(animate); //Calls the animate function again and again
+}
